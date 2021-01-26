4d59ce931d1a531cda8910dae615a2966c78112c
jonasbfranco <jonasbfranco@hotmail.com>
2021-01-26 10:03:00 -0300

    Upload files

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,2 @@
+<h1>Pagina de Contato</h1>
+<a href="/">Voltar para Home</a>
---
+++ resources/views/products.blade.php
@@ -0,0 +1,4 @@
+<h1>Pagina de Produtos</h1>
+<a href="/">Voltar para Home</a>
+<br>
+<a href="/contact">Voltar para Contato</a>
